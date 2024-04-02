5645db4751d218410f1c020ee2eab2d3ba7ad566
Bakhtawar4256 <moeenabbasi719@gmail.com>
2024-04-02 22:37:21 +0500

    Day1 to Day10 challenge

---
+++ main.ts
@@ -0,0 +1,15 @@
+// Q:1
+//Install Node.js, TypeScript and VS Code on your computer.
+//Answer: Done
+
+// Q:2
+//Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”
+let Name: string = "Bakhtawar";
+console.log(`Hello ${Name}, would you like to learn some Typescript Today?`);
+
+// Q:3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let variable: string = "Bakhtawar";
+console.log(variable.toLowerCase());
+console.log(variable.toUpperCase())
+console.log(variable.charAt(0).toUpperCase()+ variable.slice(1).toLowerCase());
